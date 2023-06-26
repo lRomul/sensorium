292ebbc9f7ce2a002affe6c83728d83d4f337bf6
Ruslan Baikulov <ruslan1123@gmail.com>
2023-06-26 13:22:16 +0300

    feat: Add train and val datasets feat: Add stack indexes generator

--- src/datasets.py
+++ src/datasets.py
@@ -1,19 +1,25 @@
 import abc
+import random
 
 import deeplake
 import numpy as np
 
+import torch
 from torch.utils.data import Dataset
 
+from src.utils import set_random_seed
 from src.frames import FramesProcessor
 from src.responses import ResponsesProcessor
+from src.indexes import StackIndexesGenerator
 
 
 class MouseVideoDataset(Dataset, metaclass=abc.ABCMeta):
     def __init__(self,
                  deeplake_path: str,
+                 indexes_generator: StackIndexesGenerator,
                  frames_processor: FramesProcessor,
                  responses_processor: ResponsesProcessor):
+        self.indexes_generator = indexes_generator
         self.frames_processor = frames_processor
         self.responses_processor = responses_processor
 
@@ -47,15 +53,62 @@ class MouseVideoDataset(Dataset, metaclass=abc.ABCMeta):
         return input_tensor, target_tensor
 
     @abc.abstractmethod
-    def __len__(self):
+    def __len__(self) -> int:
         pass
 
     @abc.abstractmethod
     def get_frame_indexes(self, index: int) -> tuple[int, list[int]]:
         pass
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> tuple[torch.Tensor, torch.Tensor]:
         video_index, frame_indexes = self.get_frame_indexes(index)
+        print(video_index, frame_indexes)
         frames, responses = self.get_frames_responses(video_index, frame_indexes)
         frames, responses = self.process_frames_responses(frames, responses)
         return frames, responses
+
+
+class TrainMouseVideoDataset(MouseVideoDataset):
+    def __init__(self,
+                 deeplake_path: str,
+                 indexes_generator: StackIndexesGenerator,
+                 frames_processor: FramesProcessor,
+                 responses_processor: ResponsesProcessor,
+                 epoch_size: int):
+        super().__init__(deeplake_path, indexes_generator, frames_processor, responses_processor)
+        self.epoch_size = epoch_size
+
+    def __len__(self) -> int:
+        return self.epoch_size
+
+    def get_frame_indexes(self, index: int) -> tuple[int, list[int]]:
+        set_random_seed(index)
+        video_index = random.randrange(0, self.num_videos)
+        frame_index = random.randrange(
+            self.indexes_generator.behind,
+            self.num_frames - self.indexes_generator.ahead
+        )
+        frame_indexes = self.indexes_generator.make_stack_indexes(frame_index)
+        return video_index, frame_indexes
+
+
+class ValMouseVideoDataset(MouseVideoDataset):
+    def __init__(self,
+                 deeplake_path: str,
+                 indexes_generator: StackIndexesGenerator,
+                 frames_processor: FramesProcessor,
+                 responses_processor: ResponsesProcessor):
+        super().__init__(deeplake_path, indexes_generator, frames_processor, responses_processor)
+        self.window_size = self.indexes_generator.ahead + self.indexes_generator.behind + 1
+        self.samples_per_video = self.num_frames // self.window_size
+
+    def __len__(self) -> int:
+        return self.num_videos * self.samples_per_video
+
+    def get_frame_indexes(self, index: int) -> tuple[int, list[int]]:
+        assert 0 <= index < self.__len__()
+        video_index = index // self.samples_per_video
+        video_sample_index = index - video_index * self.samples_per_video
+        frame_index = self.indexes_generator.behind + video_sample_index * self.window_size
+        frame_indexes = self.indexes_generator.make_stack_indexes(frame_index)
+        return video_index, frame_indexes
---
+++ src/utils.py
@@ -0,0 +1,10 @@
+import time
+import random
+
+import numpy as np
+
+
+def set_random_seed(index: int):
+    seed = int(time.time() * 1000.0) + index
+    random.seed(seed)
+    np.random.seed(seed % (2 ** 32 - 1))
