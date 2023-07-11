2efc67986c61cf8d662c8b304b1c37ff7b7ac534
Ruslan Baikulov <ruslan1123@gmail.com>
2023-07-11 04:31:19 +0300

    feat: Find best model from experiment folder

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
+import re
 import time
 import random
+from pathlib import Path
 
 import numpy as np
 
@@ -12,3 +14,27 @@ def set_random_seed(index: int):
 
 def get_lr(base_lr: float, batch_size: int, base_batch_size: int = 4) -> float:
     return base_lr * (batch_size / base_batch_size)
+
+
+def get_best_model_path(dir_path, return_score=False, more_better=True):
+    dir_path = Path(dir_path)
+    model_scores = []
+    for model_path in dir_path.glob('*.pth'):
+        score = re.search(r'-(\d+(?:\.\d+)?).pth', str(model_path))
+        if score is not None:
+            score = float(score.group(0)[1:-4])
+            model_scores.append((model_path, score))
+
+    if not model_scores:
+        if return_score:
+            return None, -np.inf if more_better else np.inf
+        else:
+            return None
+
+    model_score = sorted(model_scores, key=lambda x: x[1], reverse=more_better)
+    best_model_path = model_score[0][0]
+    if return_score:
+        best_score = model_score[0][1]
+        return best_model_path, best_score
+    else:
+        return best_model_path
