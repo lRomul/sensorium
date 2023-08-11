6988eb8ec742e661988ab7be499693e4a821d6b7
Ruslan Baikulov <ruslan1123@gmail.com>
2023-08-11 03:00:25 +0300

    Train experiment mweightedloss_001 feat: Add MultiWeightedMiceLoss feat: Use combination of poisson and correlation losses

--- configs/mweightedloss_001.py
+++ configs/mweightedloss_001.py
@@ -30,8 +30,12 @@ config = dict(
             "readout_groups": 4,
             "dropout": 0.2,
         }),
-        "loss": ("mice_correlation", {
-            "eps": 1e-8,
+        "loss": ("multi_weighted_mice", {
+            "losses_params": [
+                ("poisson", {"log_input": False, "full": False, "eps": 1e-8}),
+                ("correlation", {"eps": 1e-8}),
+            ],
+            "multipliers": [0.005, 1.0],
         }),
         "optimizer": ("AdamW", {
             "lr": get_lr(base_lr, batch_size),
