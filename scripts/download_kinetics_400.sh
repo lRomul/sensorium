3379b98e01d396566cb6a5d12f69161295c5461f
Ruslan Baikulov <ruslan1123@gmail.com>
2023-09-07 20:52:35 +0300

    feat: Download splits individually

--- scripts/download_kinetics_400.sh
+++ scripts/download_kinetics_400.sh
@@ -1,90 +1,43 @@
 #!/bin/bash
+# Source: https://github.com/cvdfoundation/kinetics-dataset
+
+# Splits: train, val, test
+split=${1}
 
 # Download directories vars
-root_dl="/workdir/data/kinetics_400"
-root_dl_targz="/workdir/data/kinetics_400_targz"
+root_dl=${2:-/workdir/data/kinetics_400}
+root_dl_targz="${root_dl}_targz"
+
+echo -e "\nDownload $split kinetics400 to '$root_dl'"
 
 # Make root directories
-[ ! -d $root_dl ] && mkdir $root_dl
-[ ! -d $root_dl_targz ] && mkdir $root_dl_targz
+[ ! -d "$root_dl" ] && mkdir "$root_dl"
+[ ! -d "$root_dl_targz" ] && mkdir "$root_dl_targz"
 
-# Download train tars, will resume
-curr_dl=${root_dl_targz}/train
-url=https://s3.amazonaws.com/kinetics/400/train/k400_train_path.txt
-[ ! -d $curr_dl ] && mkdir -p $curr_dl
-wget -c -i $url -P $curr_dl
-
-# Download validation tars, will resume
-curr_dl=${root_dl_targz}/val
-url=https://s3.amazonaws.com/kinetics/400/val/k400_val_path.txt
-[ ! -d $curr_dl ] && mkdir -p $curr_dl
-wget -c -i $url -P $curr_dl
-
-# Download test tars, will resume
-curr_dl=${root_dl_targz}/test
-url=https://s3.amazonaws.com/kinetics/400/test/k400_test_path.txt
-[ ! -d $curr_dl ] && mkdir -p $curr_dl
-wget -c -i $url -P $curr_dl
-
-# Download replacement tars, will resume
-curr_dl=${root_dl_targz}/replacement
-url=https://s3.amazonaws.com/kinetics/400/replacement_for_corrupted_k400.tgz
-[ ! -d $curr_dl ] && mkdir -p $curr_dl
-wget -c $url -P $curr_dl
+# Download tars, will resume
+curr_dl="${root_dl_targz}/${split}"
+url="https://s3.amazonaws.com/kinetics/400/${split}/k400_${split}_path.txt"
+[ ! -d "$curr_dl" ] && mkdir -p "$curr_dl"
+wget -c -i "$url" -P "$curr_dl"
 
 # Download annotations csv files
 curr_dl=${root_dl}/annotations
-url_tr=https://s3.amazonaws.com/kinetics/400/annotations/train.csv
-url_v=https://s3.amazonaws.com/kinetics/400/annotations/val.csv
-url_t=https://s3.amazonaws.com/kinetics/400/annotations/test.csv
-[ ! -d $curr_dl ] && mkdir -p $curr_dl
-wget -c $url_tr -P $curr_dl
-wget -c $url_v -P $curr_dl
-wget -c $url_t -P $curr_dl
+url="https://s3.amazonaws.com/kinetics/400/annotations/${split}.csv"
+[ ! -d "$curr_dl" ] && mkdir -p "$curr_dl"
+wget -c "$url" -P "$curr_dl"
 
 # Downloads complete
 echo -e "\nDownloads complete!"
 
-# Extract train
-curr_dl=$root_dl_targz/train
-curr_extract=$root_dl/train
-[ ! -d $curr_extract ] && mkdir -p $curr_extract
-tar_list=$(ls $curr_dl)
+# Extract
+curr_dl="${root_dl_targz}/${split}"
+curr_extract="${root_dl}/${split}"
+[ ! -d "$curr_extract" ] && mkdir -p "$curr_extract"
+tar_list=$(ls "$curr_dl")
 for f in $tar_list
 do
-	[[ $f == *.tar.gz ]] && echo Extracting $curr_dl/$f to $curr_extract && tar zxf $curr_dl/$f -C $curr_extract
-done
-
-# Extract validation
-curr_dl=$root_dl_targz/val
-curr_extract=$root_dl/val
-[ ! -d $curr_extract ] && mkdir -p $curr_extract
-tar_list=$(ls $curr_dl)
-for f in $tar_list
-do
-	[[ $f == *.tar.gz ]] && echo Extracting $curr_dl/$f to $curr_extract && tar zxf $curr_dl/$f -C $curr_extract
-done
-
-# Extract test
-curr_dl=$root_dl_targz/test
-curr_extract=$root_dl/test
-[ ! -d $curr_extract ] && mkdir -p $curr_extract
-tar_list=$(ls $curr_dl)
-for f in $tar_list
-do
-	[[ $f == *.tar.gz ]] && echo Extracting $curr_dl/$f to $curr_extract && tar zxf $curr_dl/$f -C $curr_extract
-done
-
-# Extract replacement
-curr_dl=$root_dl_targz/replacement
-curr_extract=$root_dl/replacement
-[ ! -d $curr_extract ] && mkdir -p $curr_extract
-tar_list=$(ls $curr_dl)
-for f in $tar_list
-do
-	[[ $f == *.tgz ]] && echo Extracting $curr_dl/$f to $curr_extract && tar zxf $curr_dl/$f -C $curr_extract
+	[[ $f == *.tar.gz ]] && echo Extracting "$curr_dl/$f" to "$curr_extract" && tar zxf "$curr_dl/$f" -C "$curr_extract"
 done
 
 # Extraction complete
-rm -rf $root_dl_targz
 echo -e "\nExtractions complete!"
