341b198b269d21631601f750243ec1d492f81834
Ruslan Baikulov <ruslan1123@gmail.com>
2023-09-09 02:17:14 +0300

    feat: Download kinetics replacement split

--- scripts/download_kinetics_400.sh
+++ scripts/download_kinetics_400.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Source: https://github.com/cvdfoundation/kinetics-dataset
 
-# Splits: train, val, test
+# Splits: train, val, test, replacement
 split=${1}
 
 # Download directories vars
@@ -14,17 +14,24 @@ echo -e "\nDownload $split kinetics400 to '$root_dl'"
 [ ! -d "$root_dl" ] && mkdir "$root_dl"
 [ ! -d "$root_dl_targz" ] && mkdir "$root_dl_targz"
 
-# Download tars, will resume
+# Download tars
 curr_dl="${root_dl_targz}/${split}"
-url="https://s3.amazonaws.com/kinetics/400/${split}/k400_${split}_path.txt"
 [ ! -d "$curr_dl" ] && mkdir -p "$curr_dl"
-wget -c -i "$url" -P "$curr_dl"
+if [[ "$split" == "replacement" ]]; then
+  url="https://s3.amazonaws.com/kinetics/400/replacement_for_corrupted_k400.tgz"
+  wget -c "$url" -P "$curr_dl"
+else
+  url="https://s3.amazonaws.com/kinetics/400/${split}/k400_${split}_path.txt"
+  wget -c -i "$url" -P "$curr_dl"
+fi
 
 # Download annotations csv files
-curr_dl=${root_dl}/annotations
-url="https://s3.amazonaws.com/kinetics/400/annotations/${split}.csv"
-[ ! -d "$curr_dl" ] && mkdir -p "$curr_dl"
-wget -c "$url" -P "$curr_dl"
+if [[ "$split" != "replacement" ]]; then
+  curr_dl=${root_dl}/annotations
+  url="https://s3.amazonaws.com/kinetics/400/annotations/${split}.csv"
+  [ ! -d "$curr_dl" ] && mkdir -p "$curr_dl"
+  wget -c "$url" -P "$curr_dl"
+fi
 
 # Downloads complete
 echo -e "\nDownloads complete!"
@@ -36,7 +43,7 @@ curr_extract="${root_dl}/${split}"
 tar_list=$(ls "$curr_dl")
 for f in $tar_list
 do
-	[[ $f == *.tar.gz ]] && echo Extracting "$curr_dl/$f" to "$curr_extract" && tar zxf "$curr_dl/$f" -C "$curr_extract"
+	[[ $f == *gz ]] && echo Extracting "$curr_dl/$f" to "$curr_extract" && tar zxf "$curr_dl/$f" -C "$curr_extract" && rm -f "$curr_dl/$f"
 done
 
 # Extraction complete
