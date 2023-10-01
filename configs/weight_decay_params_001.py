508f1a66e2836a6181790d19a0aa9ab660a1784b
Ruslan Baikulov <ruslan1123@gmail.com>
2023-10-01 08:43:41 +0300

    Train experiment weight_decay_params_001

--- configs/weight_decay_params_001.py
+++ configs/weight_decay_params_001.py
@@ -44,7 +44,7 @@ config = dict(
         }),
         "optimizer": ("AdamW", {
             "lr": get_lr(base_lr, batch_size),
-            "weight_decay": 0.05,
+            "weight_decay": 0.06,
         }),
         "device": "cuda:0",
         "frame_stack": {
